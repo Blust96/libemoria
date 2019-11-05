a42394bb638133c72c8596de2f96b949322fa2ed
Quentin Bohain <q.bohain@pegase-healthcare.com>
2019-11-05 19:06:52 +0100

    [ADD] Using react-router-dom to display home page and not found page components

---
+++ src/components/layout/LoadingView.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const LoadingView = () => (
+    <Fragment>
+        <p>Loading ...</p>
+    </Fragment>
+);
+
+export default LoadingView; 
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const NotFound = () => (
+    <Fragment>
+        <p>La page n'existe pas !</p>
+    </Fragment>
+);
+
+export default NotFound;
