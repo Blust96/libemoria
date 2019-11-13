ddc21d4a0e3d0cbf15ea41897611419bfadd3ba6
Quentin Bohain <q.bohain@pegase-healthcare.com>
2019-11-13 16:04:57 +0100

    [UPDATE] Getting books list from PouchDB using book's context

--- src/components/books/BookCard.js
+++ src/components/books/BookCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const BookCard = ({ book: { _id, title, author, genre } }) => (
-    // TODO: Set book in BookState to get data in BookDetails
     <Link to={`/details/${_id}`}>
         <h1>{ title }</h1>
         <h2>{ author }</h2>
