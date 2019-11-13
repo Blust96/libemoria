fb695d283f9641a178c76fe19ffe757944fdc0ae
Quentin Bohain <q.bohain@pegase-healthcare.com>
2019-11-13 17:00:03 +0100

    [ADD] BookUpdate route/component creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Books from './components/books/Books';
 import CreateBook from './components/books/CreateBook';
 import BookDetails from './components/books/BookDetails';
+import BookUpdate from './components/books/BookUpdate';
 import NotFound from './components/pages/NotFound';
 
 import BookState from './context/book/BookState';
@@ -15,10 +16,13 @@ function App() {
         <BookState>
             <Router>
                 <div className="App">
+                    <Link to='/'>Accueil</Link>
+                    <Link to='/create'>Créer</Link>
                     <Switch>
                         <Route exact path='/' component={Books} />
                         <Route exact path='/create' component={CreateBook} />
                         <Route exact path='/details/:id' component={BookDetails} />
+                        <Route exact path='/update/:id' component={BookUpdate} />
                         <Route component={NotFound} />
                     </Switch>
                 </div>
---
+++ src/components/books/BookUpdate.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useContext, useEffect } from 'react';
+
+import LoadingView from '../layout/LoadingView';
+
+import BookContext from '../../context/book/BookContext';
+
+const BookUpdate = ({ match }) => {
+
+    // Get books context
+    const bookContext = useContext(BookContext);
+    const { isLoading, book, setBook } = bookContext;
+
+    useEffect(() => {
+        setBook(match.params.id)
+    }, []);
+
+    const {
+        title,
+        author,
+        genre
+    } = book;
+
+    if (isLoading) 
+        return <LoadingView />
+    else {
+        return (
+            <Fragment>
+                <h1>{ title }</h1>
+                <h2>{ author }</h2>
+                <h3>{ genre }</h3>
+            </Fragment>
+        );
+    }
+
+}
+
+export default BookUpdate;
