935a49e6fd9a98eb8c59888765e425591ef931cc
Bohain <q.bohain@gmail.com>
2020-02-23 10:43:48 +0100

    [UPDATE] Linking book context to CreateBook component

--- src/components/books/CreateBook.js
+++ src/components/books/CreateBook.js
@@ -1,29 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 
+import BookContext from '../../context/book/BookContext';
+
 const CreateBook = () => {
+
+    // Get books context
+    const bookContext = useContext(BookContext);
+    const { books, addBook } = bookContext;
 
     // React hook form declarations
     const { register, handleSubmit, errors } = useForm();
 
     // onSubmit function
     const onSubmit = values => {
-        console.log(values);
+        addBook(books, values);
     }
 
     return <Fragment>
         <h1>Créer un livre !</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="title">Titre du livre</label>
             <input name="title" placeholder="Titre du livre" ref={register({ required: true })} />
+            <label htmlFor="author">Auteur du livre</label>
             <input name="author" placeholder="Auteur du livre" ref={register({ required: true })} />
             <label htmlFor="genre">Genre</label>
             <select name="genre" placeholder="Genre" ref={register({ required: true })}>
-                <option value="male">male</option>
-                <option value="female">female</option>
+                <option value="science_fiction">Science-fiction</option>
+                <option value="policier">Policier</option>
+                <option value="thriller">Thriller</option>
+                <option value="manga">Manga</option>
             </select>
-            <input name="isbn" placeholder="ISBN" ref={register({ required: true })} />
-            <input name="description" placeholder="Description" ref={register({ required: true })} />
-            <input name="coverPath" placeholder="Couverture" ref={register({ required: true })} />
+            <label htmlFor="isbn">ISBN</label>
+            <input name="isbn" placeholder="ISBN" ref={register} /> { /* register({ pattern: /^(97(8|9))?\d{9}(\d|X)$/i }) */ }
+            <label htmlFor="description">Description</label>
+            <input name="description" placeholder="Description" ref={register} />
+            <label htmlFor="coverPath">Couverture du livre</label>
+            <input name="coverPath" placeholder="Couverture" ref={register} />
             <label htmlFor="favorite">Favoris</label>
             <input type="checkbox" name="favorite" ref={register} />
             <label htmlFor="read">Lu</label>
