f28109ae9009aa6b3074cae912a595ea0e7253a8
Quentin <q.bohain@gmail.com>
2020-02-24 13:16:59 +0100

    [UPDATE] Removing useless props

--- src/components/books/BookCard.js
+++ src/components/books/BookCard.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React,  { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-const BookCard = ({ book: { _id, title, author, genre } }) => (
-    <Link to={`/details/${_id}`}>
-        <h1>{ title }</h1>
-        <h2>{ author }</h2>
-        <p>{ genre }</p>
-    </Link>
+const BookCard = ({ book: { _id, title, author, genre, coverPath, favorite, read, wish } }) => (
+    <Fragment>
+        <Link to={`/details/${_id}`}>
+            { 
+                coverPath
+                ? (<img src={coverPath}></img>)
+                : (<div style={{ width: '80px', height: '140px', backgroundColor: '#000' }}></div>)
+            }
+            <h2>{ title }</h2>
+            <p className='bookGenre'>{ genre }</p>
+            <h3>{ author }</h3>
+            <div className='badges'>
+                { favorite ? (<div style={{ width: '25px', height: '25px', backgroundColor: '#000' }}></div>) : '' }
+                { read ? (<div style={{ width: '25px', height: '25px', backgroundColor: '#000' }}></div>) : '' }
+                { wish ? (<div style={{ width: '25px', height: '25px', backgroundColor: '#000' }}></div>) : '' }
+            </div>
+        </Link>
+    </Fragment>
 );
 
 export default BookCard;
