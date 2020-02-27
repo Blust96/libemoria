6d67391a95863ba6494be4a7ceece2cfee7a8403
Quentin <q.bohain@gmail.com>
2020-02-27 15:04:05 +0100

    [UPDATE] Title as cover image alt

--- src/components/books/BookCard.js
+++ src/components/books/BookCard.js
@@ -6,7 +6,7 @@ const BookCard = ({ book: { _id, title, author, genre, coverPath, favorite, read
         <Link to={`/details/${_id}`}>
             { 
                 coverPath
-                ? (<img src={coverPath}></img>)
+                ? (<img src={coverPath} alt={title}></img>)
                 : (<div style={{ width: '80px', height: '140px', backgroundColor: '#000' }}></div>)
             }
             <h2>{ title }</h2>
